f47f56f281f5727c359ca0b38d8b55ebe04c0cdb
hoke946 <44010396+hoke946@users.noreply.github.com>
2021-02-04 17:39:28 +0900

    親子操作追加、README追加

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Trigger2to3
-An alternative to VRC_Trigger that can be used with VRChat SDK3
+VRChat SDK3でVRC_Triggerを使いたい人のための代替ツールです。  
+VRC_Triggerの機能の大部分を取り込み、VRC_Triggerに近い使用感でワールドギミックを組み立てることができます。  
+VRC_TriggerのUIを次のような3つのUdonで表現して、実現します。  
+  
+![T23pic](https://user-images.githubusercontent.com/44010396/106849398-15e22300-665f-11eb-9408-a473315cc608.png)  
+  
+## ダウンロード
+  
+[こちら](https://github.com/hoke946/Trigger2to3/releases)から入手できます。
+  
+## 導入方法・使用方法
+[こちら](https://docs.google.com/document/d/1WEZ70eKVvkrF3LpBr7qmxQmNMBagdtocgVIN_ayhGLM/edit?usp=sharing)を参照してください。
+  
+## お詫びとお願い
+当ツールはベータ版のため、不具合が発生する可能性が高いです。  
+使ってみた感想、問題点、ご意見やご要望などありましたら、  
+Twitterにて次のハッシュタグでお寄せください。⇒ #Trigger2to3  
+または、[@hoke946](https://twitter.com/hoke946)への直リプやDMでも問題ありません。  
+  
+## ライセンス
+MIT Licenseで運用します。  
+当ツールを組み込んだギミックの配布も可能です。  
+その際はLICENSEファイルの組み込みと、こちらへのリンクをお願いします。  
---
+++ Script/Action/T23_SetChildrenActive.cs
@@ -0,0 +1,178 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class T23_SetChildrenActive : UdonSharpBehaviour
+{
+    public int groupID;
+
+    [SerializeField]
+    private GameObject[] recievers;
+
+    [SerializeField]
+    private bool operation;
+
+    [SerializeField]
+    private bool takeOwnership;
+
+    private bool executing = false;
+    private bool[] executed;
+    private float waitTimer;
+
+    [SerializeField, Range(0, 1)]
+    private float randomAvg;
+
+    private float randomMin = 0;
+    private float randomMax = 0;
+
+    private T23_BroadcastLocal broadcastLocal;
+    private T23_BroadcastGrobal broadcastGrobal;
+
+    void Start()
+    {
+        T23_BroadcastLocal[] broadcastLocals = GetComponents<T23_BroadcastLocal>();
+        for (int i = 0; i < broadcastLocals.Length; i++)
+        {
+            if (broadcastLocals[i].groupID == groupID)
+            {
+                broadcastLocal = broadcastLocals[i];
+                break;
+            }
+        }
+
+        if (broadcastLocal)
+        {
+            broadcastLocal.AddActions(this);
+
+            if (broadcastLocal.randomize)
+            {
+                randomMin = broadcastLocal.randomTotal;
+                broadcastLocal.randomTotal += randomAvg;
+                randomMax = broadcastLocal.randomTotal;
+            }
+        }
+        else
+        {
+            T23_BroadcastGrobal[] broadcastGrobals = GetComponents<T23_BroadcastGrobal>();
+            for (int i = 0; i < broadcastGrobals.Length; i++)
+            {
+                if (broadcastGrobals[i].groupID == groupID)
+                {
+                    broadcastGrobal = broadcastGrobals[i];
+                    break;
+                }
+            }
+
+            if (broadcastGrobal)
+            {
+                broadcastGrobal.AddActions(this);
+
+                if (broadcastGrobal.randomize)
+                {
+                    randomMin = broadcastGrobal.randomTotal;
+                    broadcastGrobal.randomTotal += randomAvg;
+                    randomMax = broadcastGrobal.randomTotal;
+                }
+            }
+        }
+
+#if UNITY_EDITOR
+        // local simulation
+        takeOwnership = false;
+#endif
+    }
+
+    void Update()
+    {
+        if (executing)
+        {
+            bool failure = false;
+            for (int i = 0; i < recievers.Length; i++)
+            {
+                if (recievers[i])
+                {
+                    if (!executed[i])
+                    {
+                        if (Networking.IsOwner(recievers[i]))
+                        {
+                            Execute(recievers[i]);
+                            executed[i] = true;
+                        }
+                        else
+                        {
+                            failure = true;
+                        }
+                    }
+                }
+            }
+
+            if (!failure)
+            {
+                executing = false;
+            }
+
+            waitTimer += Time.deltaTime;
+            if (waitTimer > 5)
+            {
+                executing = false;
+            }
+        }
+    }
+
+    public void Action()
+    {
+        if (!RandomJudgement()) { return; }
+
+        for (int i = 0; i < recievers.Length; i++)
+        {
+            if (recievers[i])
+            {
+                if (takeOwnership)
+                {
+                    Networking.SetOwner(Networking.LocalPlayer, recievers[i]);
+                    for (int cidx = 0; cidx < recievers[i].transform.childCount; cidx++)
+                    {
+                        Networking.SetOwner(Networking.LocalPlayer, recievers[i].transform.GetChild(cidx).gameObject);
+                    }
+                    executing = true;
+                    executed = new bool[recievers.Length];
+                    waitTimer = 0;
+                }
+                else
+                {
+                    Execute(recievers[i]);
+                }
+            }
+        }
+    }
+
+    private void Execute(GameObject target)
+    {
+        for (int cidx = 0; cidx < target.transform.childCount; cidx++)
+        {
+            target.transform.GetChild(cidx).gameObject.SetActive(operation);
+        }
+    }
+
+    private bool RandomJudgement()
+    {
+        if (broadcastLocal)
+        {
+            if (!broadcastLocal.randomize || (broadcastLocal.randomValue >= randomMin && broadcastLocal.randomValue < randomMax))
+            {
+                return true;
+            }
+        }
+        else if (broadcastGrobal)
+        {
+            if (!broadcastGrobal.randomize || (broadcastGrobal.randomValue >= randomMin && broadcastGrobal.randomValue < randomMax))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
